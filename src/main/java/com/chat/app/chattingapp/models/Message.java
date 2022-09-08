7d74ed63d79fe8646db0f13a1ac22348f1147950
Subhamdas20 <Subham21.sd@gmail.com>
2022-09-08 20:23:59 +0530

    first commit

---
+++ src/main/java/com/chat/app/chattingapp/models/Message.java
@@ -0,0 +1,13 @@
+package com.chat.app.chattingapp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String name;
+    private String content;
+}
---
+++ src/test/java/com/chat/app/chattingapp/ChattingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.app.chattingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
