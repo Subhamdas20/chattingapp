02241652474592306c78ea83a9fa37898cbcd192
Subhamdas20 <Subham21.sd@gmail.com>
2022-09-09 09:07:06 +0530

    App ready

--- src/main/java/com/chat/app/chattingapp/config/Config.java
+++ src/main/java/com/chat/app/chattingapp/config/Config.java
@@ -12,13 +12,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class Config implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+
         registry.addEndpoint("/server1").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
